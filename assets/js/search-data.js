3737fb7e6e92c291da4a6e9a21defe68525e6da9
ota-semicon-lab <ota.material.lab@gmail.com>
2026-03-02 09:43:28 +0000

    Deploying to gh-pages from @ ota-semicon-lab/web@ca39f82164c260c436159a5151e41eb7c92b60d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/web/repositories/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/web/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
