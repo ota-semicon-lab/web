de5885f6e6e0981a2d0b8602ddfca717f344f427
ota-semicon-lab <ota.material.lab@gmail.com>
2026-03-02 08:19:50 +0000

    Deploying to gh-pages from @ ota-semicon-lab/web@820a026290b52d1b0b2295bc9612002da0f1daeb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-computational-materials-science-group",
+    title: "Computational Materials Science Group",
     section: "Navigation",
     handler: () => {
       window.location.href = "/web/";
