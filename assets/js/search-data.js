ffe67b5bd2d9e42fc243f8d717f41a97d2bb75a2
ota-semicon-lab <ota.yuichi@pu-toyama.ac.jp>
2026-03-23 09:49:21 +0000

    Deploying to gh-pages from @ ota-semicon-lab/web@b83f9b1a3652261c38b84347b696d33c51ec4ce3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "Projects",
-          description: "半導体を中心とした先進的な材料の研究を行っています.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/web/projects/";
