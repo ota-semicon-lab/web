be731e744d6546049d95aa7ad46360d5af2698dc
ota-semicon-lab <ota.yuichi@pu-toyama.ac.jp>
2026-03-25 05:37:53 +0000

    Deploying to gh-pages from @ ota-semicon-lab/web@17bff8ddb55f873987c5294ebffabc66529e7a20 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-太田研究室",
-    title: "太田研究室",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/web/";
