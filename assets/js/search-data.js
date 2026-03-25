139294bb764ffc0a63ecb7c9e95f9807fac86ab7
ota-semicon-lab <ota.yuichi@pu-toyama.ac.jp>
2026-03-25 04:26:46 +0000

    Deploying to gh-pages from @ ota-semicon-lab/web@f811f2ec5428e523a5413adf2c7c9263b57e4656 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -411,9 +411,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/web/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-project-3",
+          title: 'project 3',
+          description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/web/projects/3_project/";
             },},{id: "teachings-data-science-fundamentals",
